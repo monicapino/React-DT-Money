65b4c946f480311baa70d76ce143832a5df7dd83
Monica Pino <112265101+monicapino@users.noreply.github.com>
2025-01-24 15:19:48 -0300

    First commit

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,30 @@
-import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styled";
+import { HeaderContainer, HeaderContent, NewTransactionButton } from './styled'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/LogoTimer.svg'
 
 export function Header() {
-    return (
-        <HeaderContainer>
-            <HeaderContent>
-                <img src={logoImg} alt="" />
+  return (
+    <HeaderContainer>
+      <HeaderContent>
+        <img src={logoImg} alt="" />
 
-                <NewTransactionButton>Nova transação</NewTransactionButton>
-            </HeaderContent>
-        </HeaderContainer>
-    )
+        <Dialog.Root>
+          <Dialog.DialogTrigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.DialogTrigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+      </HeaderContent>
+    </HeaderContainer>
+  )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,39 +1,42 @@
-import { Header } from "../../components/Header"
-import { Summary } from "../../components/Summary"
-import { PriceHightlight, TransactionsContainer, TransactionsTable } from "./styles"
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHightlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
 
 export function Transactions() {
-    return (
-        <div>
-            <Header />
-            <Summary />
+  return (
+    <div>
+      <Header />
+      <Summary />
 
-            <TransactionsContainer>
-            <TransactionsTable>
-                <tbody>
-                    <tr>
-                        <td width="50%">Desenvolvimento de site</td>
-                        <td>
-                            <PriceHightlight variant="income">
-                                R$ 12.000,00
-                            </PriceHightlight>
-                        </td>
-                        <td>Venda</td>
-                        <td>13/04/2025</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Hamburger</td>
-                        <td>
-                            <PriceHightlight variant="outcome">
-                            - R$ 59,00
-                            </PriceHightlight>
-                        </td>
-                        <td>Alimentação</td>
-                        <td>10/01/2025</td>
-                    </tr>
-                </tbody>
-            </TransactionsTable>
-            </TransactionsContainer>
-        </div>
-    )
+      <TransactionsContainer>
+        <SearchForm />
+
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHightlight variant="income">R$ 12.000,00</PriceHightlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHightlight variant="outcome">- R$ 59,00</PriceHightlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/01/2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
 }
